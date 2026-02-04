4c6e2647b074a75d14cbc9f3a9a23fe72cf71a27
Davi da Silva <davi.dasilva@colostate.edu>
2026-02-03 23:56:53 -0700

    First commit

--- src/AtmSimulator.java
+++ src/AtmSimulator.java
@@ -1,5 +1,5 @@
 public class AtmSimulator {
     public static void main(String[] args) {
-        System.out.println("Hello, this is the first commit!");
+        System.out.println("Hello World, this is the first commit!");
     }
 }
