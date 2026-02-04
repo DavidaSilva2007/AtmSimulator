ebc0c1324d45e4f92635ba34472519d5fdbe6c2c
Davi da Silva <davi.dasilva@colostate.edu>
2026-02-04 15:38:30 -0700

    Added logIn function, and added it to main function. The program is now able to log in given a password.

--- src/AtmSimulator.java
+++ src/AtmSimulator.java
@@ -1,16 +1,45 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AtmSimulator {
+
+    // Scanner for pin input and menu.
+    static Scanner scanner = new Scanner(System.in);
+    // Runs the main menu of the program, when false, program finishes.
+    static boolean running = true;
 
     // Stores the balance of the account.
     static int balance = 10000;
     // A list of all the transactions made, able to print it.
     static ArrayList<String> transactions = new ArrayList<>();
     // Stores the user's PIN
-    static int password = 1234;
+    static int basePin = 1234;
 
     public static void main(String[] args) {
-        System.out.println("Hello World, this is the first commit!");
+        while (!logIn()) {
+            System.out.println("Invalid PIN, please try again");
+        }
+        System.out.println("You are logged in!");
+
+//        while (running) {
+//            int menuChoice = mainUI();
+//
+//            switch (menuChoice) {
+//                case 1:
+//                    showBalance();
+//                case 2:
+//                    deposit();
+//                case 3:
+//                    withdraw();
+//                case 4:
+//                    showTransactionHistory();
+//                case 5:
+//                    logOut();
+//                    break;
+//                default:
+//                   System.out.println("Invalid menu choice, please try again.");
+//            }
+//        }
     }
 
     // Stores options for main in a list, and displays it.
@@ -19,13 +48,21 @@ public class AtmSimulator {
     }
 
     // Checks for password, and lets user log into the account.
-    public static boolean LogIn() {
-        return true;
+    public static boolean logIn() {
+        System.out.print("Please enter your pin: ");
+        int pinInput = scanner.nextInt();
+
+        return pinInput == basePin;
+
     }
 
     // Logs user out of the account.
-    public static boolean logOut() {
-        return false;
+    public static void logOut() {
+        System.out.println("Thank you! Exiting...");
+    }
+
+    // Allows user to change basePin, while in the program.
+    public static void changePin() {
     }
 
     // Prints user's current balance.
@@ -33,11 +70,11 @@ public class AtmSimulator {
     }
 
     // Allows user to input deposit amount into the account.
-    public static void deposit(int balance) {
+    public static void deposit() {
     }
 
     // Allows user to input withdraw amount out of the account.
-    public static void withdraw(int balance) {
+    public static void withdraw() {
     }
 
     // Prints deposits and withdrawals in transactions list.
