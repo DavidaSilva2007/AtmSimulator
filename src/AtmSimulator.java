b3ed15c5fa08632ca93410e69ac9e502a3ec8cb4
Davi da Silva <davi.dasilva@colostate.edu>
2026-02-06 12:06:27 -0700

    Functions changePin needs debugging. Also the outputs need to be formatted to have a new line after PIN inputs so that the input is directly next to the print, but after that there is a new line.

--- src/AtmSimulator.java
+++ src/AtmSimulator.java
@@ -21,25 +21,34 @@ public class AtmSimulator {
         }
         System.out.println("You are logged in!");
 
-//        while (running) {
-//            int menuChoice = mainUI();
-//
-//            switch (menuChoice) {
-//                case 1:
-//                    showBalance();
-//                case 2:
-//                    deposit();
-//                case 3:
-//                    withdraw();
-//                case 4:
-//                    showTransactionHistory();
-//                case 5:
-//                    logOut();
-//                    break;
-//                default:
-//                   System.out.println("Invalid menu choice, please try again.");
-//            }
-//        }
+        changePin();
+        System.out.println(basePin);
+
+        while (!running) {
+            int menuChoice = mainUI();
+
+            switch (menuChoice) {
+                case 1:
+                    showBalance();
+                case 2:
+                    deposit();
+                case 3:
+                    withdraw();
+                case 4:
+                    changePin();
+                case 5:
+                    showTransactionHistory();
+                case 6:
+                    logOut();
+                    break;
+                default:
+                   System.out.println("Invalid menu choice, please try again.");
+            }
+        }
+    }
+
+    public static void newLine() {
+        System.out.println();
     }
 
     // Stores options for main in a list, and displays it.
@@ -49,11 +58,11 @@ public class AtmSimulator {
 
     // Checks for password, and lets user log into the account.
     public static boolean logIn() {
-        System.out.print("Please enter your pin: ");
+        System.out.print("Please enter your PIN: ");
         int pinInput = scanner.nextInt();
+        newLine();
 
         return pinInput == basePin;
-
     }
 
     // Logs user out of the account.
@@ -63,6 +72,50 @@ public class AtmSimulator {
 
     // Allows user to change basePin, while in the program.
     public static void changePin() {
+        while (true) {
+            System.out.print("Please enter your current PIN: ");
+            newLine();
+
+            if (!scanner.hasNextInt()) {
+
+                System.out.println("Invalid PIN, please try again.");
+                continue;
+            }
+
+            int pinInput = scanner.nextInt();
+            scanner.next();
+
+            if (pinInput != basePin) {
+
+                System.out.println("Incorrect PIN, please try again.");
+                continue;
+            }
+
+            while (true) {
+                System.out.print("Enter a new PIN: ");
+                newLine();
+
+                if (!scanner.hasNextInt()) {
+
+                    System.out.println("Invalid PIN, please try again.");
+                    continue;
+                }
+
+                int pinChange = scanner.nextInt();
+                scanner.next();
+
+                if (pinChange >= 1000 && pinChange <= 9999) {
+                    basePin = pinChange;
+
+                    System.out.println("PIN changed successfully.");
+                    return;
+                } else {
+
+                    System.out.println("PIN must be exactly 4 digits.");
+                    continue;
+                }
+            }
+        }
     }
 
     // Prints user's current balance.
